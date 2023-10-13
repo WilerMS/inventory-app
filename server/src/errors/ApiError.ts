b5a435e85014b17afd71fe85f89b2ff6c1e0a525
WilerMS <wilersoriano@gmail.com>
2023-10-13 11:14:58 +0200

    Errors created

---
+++ server/src/errors/ApiError.ts
@@ -0,0 +1,9 @@
+export abstract class ApiError extends Error {
+  status: number
+
+  constructor (status: number, message: string, name: string) {
+    super(message)
+    this.name = name
+    this.status = status
+  }
+}
---
+++ server/src/errors/index.ts
@@ -0,0 +1,6 @@
+export { ApiError } from './ApiError'
+export { default as BadRequestError } from './BadRequestError'
+export { default as ForbiddenError } from './ForbiddenError'
+export { default as InternalServerError } from './InternalServerError'
+export { default as NotFoundError } from './NotFoundError'
+export { default as UnauthorizedError } from './UnauthorizedError'
