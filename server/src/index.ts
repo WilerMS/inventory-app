9d64cd8bbeea3678a82256a9be0bfd1a281c3353
WilerMS <wilersoriano@gmail.com>
2023-10-13 11:14:50 +0200

    Server initialized

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello server!')
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+import { PORT } from './constants/env'
+
+const app = express()
+
+// middlewares
+app.use(morgan('dev'))
+app.use(cors())
+
+// routes
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`)
+})
