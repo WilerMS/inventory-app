2d55b55f74655093a2cb809c56d6fe978d7c840f
WilerMS <wilersoriano@gmail.com>
2023-10-15 00:29:06 +0200

    error managements to intercept all errors in routes

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,25 +1,36 @@
-import {
-  type Request,
-  type Response,
-  type NextFunction
-} from 'express'
+import { type Request, type Response, type NextFunction } from 'express'
 
 import { ApiError } from '@/errors'
+import { ValidationError } from 'objection'
 
-export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const errorMiddleware = (
+  err: any,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof ApiError) {
-    res
+    return res
       .status(err.status)
       .json({
         error: err.name,
-        message: err.message
-      })
-  } else {
-    res
-      .status(500)
-      .json({
-        error: 'InternalServerError',
-        message: 'Something went wrong!'
+        message: err.message,
+        details: err.details
       })
   }
+
+  if (err instanceof ValidationError) {
+    res.status(400).json({
+      error: ValidationError.name,
+      message: err.message,
+      details: err.data
+    })
+  }
+
+  res
+    .status(500)
+    .json({
+      error: 'InternalServerError',
+      message: 'Something went wrong!'
+    })
 }
