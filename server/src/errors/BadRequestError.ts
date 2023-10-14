31035b54cec1e59c8eae40a04d1274a590e13175
WilerMS <wilersoriano@gmail.com>
2023-10-15 00:28:38 +0200

    add details props for errors

--- server/src/errors/BadRequestError.ts
+++ server/src/errors/BadRequestError.ts
@@ -1,7 +1,7 @@
 import { ApiError } from './ApiError'
 
 export default class BadRequestError extends ApiError {
-  constructor (message: string = 'Bad Request') {
-    super(400, message, 'BadRequest')
+  constructor (message: string = 'Bad Request', details: object | string = {}) {
+    super(400, message, 'BadRequest', details)
   }
 }
