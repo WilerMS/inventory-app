d80bf66e10a9f8d86e7e34f917cf7530171f072b
WilerMS <wilersoriano@gmail.com>
2023-10-15 00:29:55 +0200

    Add logic to validate body and send error messages to client

---
+++ server/src/middlewares/validateBody.ts
@@ -0,0 +1,21 @@
+import Ajv from 'ajv'
+import { type RequestHandler } from 'express'
+import { type JSONSchema } from 'objection'
+
+import { ValidationError } from '@/errors'
+
+const ajv = new Ajv()
+
+export const validateBody = (schema: JSONSchema): RequestHandler => {
+  return (req, _, next) => {
+    const valid = ajv.validate(schema, req.body)
+
+    if (!valid) {
+      const errorArr = ajv.errors?.map(error => error.message)
+      const error = errorArr?.join(', ')
+      throw new ValidationError(error, ajv.errors as object)
+    }
+
+    next()
+  }
+}
