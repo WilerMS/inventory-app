96587033dbc540df3c43f662680c131071dbcc25
WilerMS <wilersoriano@gmail.com>
2023-10-18 02:54:31 +0200

    exporting hooks

---
+++ client/src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useAppNavigate } from './useAppNavigate'
+export { default as useAuthentication } from './useAuthentication'
+export { useDebounce } from './useDebounce'
+export { useLocalStorage } from './useLocalStorage'
