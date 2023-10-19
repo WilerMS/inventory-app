34330427d6d4a0fb8f1ba93253fb04449f96b400
WilerMS <wilersoriano@gmail.com>
2023-10-20 00:23:39 +0200

    Add files route to upload user fphotos

--- server/src/routes/files.ts
+++ server/src/routes/files.ts
@@ -1,16 +1,47 @@
-import { InternalServerError } from '@/errors'
+import { ConflictError, InternalServerError } from '@/errors'
+import { User } from '@/models/User'
+import { type AuthenticatedRequest } from '@/types'
 import { errorHandler } from '@/utils'
 import { Router } from 'express'
 import multer from 'multer'
 import path from 'path'
 
 const router = Router()
-const upload = multer({ dest: path.resolve(__dirname, '../../public/images') })
+export const upload = multer({ dest: path.resolve(__dirname, '../../public/images') })
 
-router.post('/', upload.single('file'), errorHandler((req, res) => {
+router.post('/', upload.single('file'), errorHandler((req: AuthenticatedRequest, res) => {
   const file = req.file
   if (!file) throw new InternalServerError('File has not been uploaded, please try later')
   res.send(file.filename)
 }))
+
+router.put(
+  '/user-photo',
+  upload.single('file'),
+  errorHandler(async (req: AuthenticatedRequest, res) => {
+    // @ts-expect-error
+    const { id } = req.auth.user
+
+    const file = req.file
+    if (!file) throw new InternalServerError('File has not been uploaded, please try later')
+
+    const existingUser = await User.query().findById(id)
+    if (!existingUser) {
+      throw new ConflictError('User not found')
+    }
+
+    await User
+      .query()
+      .findById(id)
+      .patch({ image: file.filename })
+
+    const updatedUser = await User.query().findById(id) as User
+
+    return res.json({
+      message: 'Image successfully updated',
+      user: updatedUser.toResponse()
+    })
+  })
+)
 
 export default router
