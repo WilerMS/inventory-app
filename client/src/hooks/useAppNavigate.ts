80ff76feb96dfa3b7b9c667027284bc9d8aa7d73
WilerMS <wilersoriano@gmail.com>
2023-10-23 16:36:47 +0200

    Add previousPath to useAppNavigate Hook

--- client/src/hooks/useAppNavigate.ts
+++ client/src/hooks/useAppNavigate.ts
@@ -1,16 +1,35 @@
 import { flushSync } from 'react-dom'
-import { useNavigate } from 'react-router-dom'
+import { type NavigateOptions, useNavigate, useLocation } from 'react-router-dom'
+
+interface NavigateOptionsProps extends NavigateOptions {
+  previousPath?: string
+}
 
 export const useAppNavigate = () => {
   const navigate = useNavigate()
+  const { pathname } = useLocation()
 
-  const _navigate = (path: any) => {
+  const _navigate = (path: any, options?: NavigateOptionsProps) => {
+    // @ts-expect-error
     if (!document.startViewTransition) {
-      navigate(path)
+      navigate(path, {
+        ...options,
+        state: {
+          previousPath: pathname,
+          ...options?.state
+        }
+      })
     } else {
+      // @ts-expect-error
       document.startViewTransition(() => {
         flushSync(() => {
-          navigate(path)
+          navigate(path, {
+            ...options,
+            state: {
+              previousPath: pathname,
+              ...options?.state
+            }
+          })
         })
       })
     }
