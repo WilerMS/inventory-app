f330950995c79f0cab358a5ac033e3a298810f7c
WilerMS <wilersoriano@gmail.com>
2023-10-24 21:02:38 +0200

    Add children to floating button

--- client/src/features/floating-btn/index.tsx
+++ client/src/features/floating-btn/index.tsx
@@ -1,20 +1,58 @@
-import { AddIcon } from '@/icons'
+import { type FC, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import cn from 'classnames'
-import { useLocation } from 'react-router-dom'
 
-export const FloatingActionButton = () => {
-  const { pathname } = useLocation()
+import { AddIcon } from '@/icons'
+import { type Children } from '@/types'
 
-  if (['/login', '/register', '/profile'].includes(pathname)) return null
+interface Props {
+  children: Children
+}
+
+const variants = {
+  open: { opacity: 1, height: 'auto' },
+  closed: { opacity: 0, height: 0 }
+}
+
+export const FloatingActionButton: FC<Props> = ({ children }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleToggleExpand = () => setExpanded(state => !state)
 
   return (
-    <button
-      className={cn(
-        'absolute bottom-10 right-4 z-50',
-        'w-[70px] h-[70px] rounded-full center bg-white shadow-lg border-2 border-slate-300'
-      )}
-    >
-      <AddIcon width={32} height={32}/>
-    </button>
+    <div className={cn('absolute bottom-10 right-4 z-50')} >
+      <button
+        onClick={handleToggleExpand}
+        className={cn(
+          'w-[70px] h-[70px] rounded-full center bg-white shadow-md border-2'
+        )}
+      >
+        <AddIcon
+          className={cn(
+            'transition-all',
+            expanded ? 'rotate-[45deg]' : ''
+          )}
+          width={32}
+          height={32}
+        />
+      </button>
+      <AnimatePresence>
+        {expanded &&
+          <motion.div
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={variants}
+            transition={{ duration: 0.30 }}
+            className={cn(
+              'mb-2 w-[220px] absolute bottom-[100%] right-0 rounded-xl',
+              'bg-white shadow-md flex flex-col overflow-hidden'
+            )}
+          >
+            {children}
+          </motion.div>
+        }
+      </AnimatePresence>
+    </div>
   )
 }
