d3e4590f5a8efb5a42a259e13e37908ae7e5e705
WilerMS <wilersoriano@gmail.com>
2023-10-26 00:51:05 +0200

    Add Zone image in zones

--- client/src/pages/Zones/Zone.tsx
+++ client/src/pages/Zones/Zone.tsx
@@ -2,35 +2,46 @@ import cn from 'classnames'
 import { motion } from 'framer-motion'
 
 import folderImage from '@/assets/folder.png'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { framerContainerVariant } from '@/constants/transition'
+import { api } from '@/services/api'
+import { useQuery } from '@tanstack/react-query'
+import { buildUrl } from '@/constants/env'
+import { type ZoneInterface } from '@/types'
+import { PenIcon } from '@/icons'
+import { LinkButton } from '@/components/lib'
 
 export default function Zone () {
   const { zoneId } = useParams()
+  const { state } = useLocation()
+  const fetchZones = async () => await api<ZoneInterface>(buildUrl(`/zones/${zoneId}`))
+  const { data: zoneFetched } = useQuery({
+    queryKey: ['zones', zoneId],
+    queryFn: fetchZones
+  })
+
+  console.log({ state })
 
   return (
     <main className='w-full h-full pt-[75px] px-4 pb-[100px] overflow-auto scroll-bar-hide relative'>
-      <section className="mb-4">
-        <div
-          className={cn(
-            'w-full h-[250px] center flex-col'
-          )}
-        >
-          <div className='border-4 border-[#81c784] aspect-square center flex-col p-10 rounded-full'>
-            <img
-              className='w-[120px]'
-              src={folderImage}
-              alt="Products"
-              style={{
-                viewTransitionName: `zone-image-${zoneId}`,
-                contain: 'layout'
-              }}
-            />
-          </div>
-        </div>
-      </section>
+     <figure className={cn('relative w-full h-[220px] center flex-col mt-4 mb-3')}>
+        <picture className=' group w-[200px] h-[200px] overflow-hidden center rounded-full'>
+          {zoneFetched?.image
+            ? <img className='w-full h-full object-cover' src={buildUrl(`/images/${zoneFetched.image}`)} />
+            : <img className='w-[120px]' src={folderImage} alt="Zone" />
+          }
+        </picture>
+      </figure>
       <section>
-        <h3 className="text-xl ml-1 mb-2 font-bold">Zone {zoneId} products:</h3>
+        <div className="flex items-center justify-between">
+          <h3 className="capitalize text-2xl ml-1 mb-2 font-bold">{zoneFetched?.name}</h3>
+          <LinkButton
+            to={`/zones/${zoneId}/edit`}
+            className='p-2 mb-1 bg-slate-700 rounded-full'
+          >
+            <PenIcon className='text-2xl' color='#fff' />
+          </LinkButton>
+        </div>
         <motion.div
           className="w-full grid grid-cols-2 gap-4"
           variants={framerContainerVariant}
