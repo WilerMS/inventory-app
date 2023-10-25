4c2363033dcfe6cf71bd3d4ff8c9a809790a45d5
WilerMS <wilersoriano@gmail.com>
2023-10-25 23:58:38 +0200

    Add Edit pen Icon

--- client/src/icons/index.tsx
+++ client/src/icons/index.tsx
@@ -177,7 +177,7 @@ export const LogoutIcon: FC<IconProps> = ({ height = '1em', width = '1em', color
     viewBox="0 0 24 24"
     fill="none"
     strokeWidth="2"
-    stroke="currentColor"
+    stroke={color ?? 'currentColor'}
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
@@ -205,5 +205,24 @@ export const CameraIcon: FC<IconProps> = ({ height = '1em', width = '1em', color
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2"></path>
     <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+  </svg>
+)
+
+export const PenIcon: FC<IconProps> = ({ height = '1em', width = '1em', color = '#1C274C', ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill="none"
+    strokeWidth="2"
+    stroke={color ?? 'currentColor'}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path>
+    <path d="M13.5 6.5l4 4"></path>
   </svg>
 )
