d4bc807ef062c5f8447b6bc7b0ce86703e9703b2
WilerMS <wilersoriano@gmail.com>
2023-10-25 22:43:20 +0200

    Add hex in getContrastColor

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -64,5 +64,5 @@ export const getContrastColor = (hexColor: `#${string}`) => {
   const b = parseInt(color.substring(4, 6), 16)
 
   const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
-  return luminance < 0.7 ? 'white' : 'black'
+  return luminance < 0.7 ? '#fff' : '#000'
 }
