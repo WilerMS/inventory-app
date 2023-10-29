e8bdef42cf8efab93dd239a8bc9cebe4cf80d028
WilerMS <wilersoriano@gmail.com>
2023-10-29 02:01:20 +0100

    Add types for products

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,3 +1,17 @@
+export interface ProductInterface {
+  id: number
+  name: string
+  image?: string
+  color?: `#${string}`
+  amount: number
+  user_id: number
+  zone_id: number | undefined
+  expiration_date?: string
+  price?: number
+  notes?: string
+  zone?: ZoneInterface
+}
+
 export interface ZoneInterface {
   id: number
   name: string
@@ -5,6 +19,7 @@ export interface ZoneInterface {
   user_id: number
   notes?: string
   color?: `#${string}`
+  products?: ProductInterface[]
 }
 
 export interface UserInterface {
