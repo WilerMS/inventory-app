f624a06586b5b5518517afbc22fc8cdb0fc50cc5
WilerMS <wilersoriano@gmail.com>
2023-11-03 04:24:32 +0100

    Add env vars

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -1,2 +1,8 @@
 export const PORT = process.env.PORT ?? 3000
 export const JWT_SECRET = process.env.JWT_SECRET ?? 'JWT_SECRET'
+
+export const OBJECTION_DB_HOST = process.env.OBJECTION_DB_HOST ?? '127.0.0.1' ?? '192.168.1.160'
+export const OBJECTION_DB_PORT = process.env.OBJECTION_DB_PORT ?? 3307
+export const OBJECTION_DB_USER = process.env.OBJECTION_DB_USER ?? 'root'
+export const OBJECTION_DB_PASSWORD = process.env.OBJECTION_DB_PASSWORD ?? 'wiler'
+export const OBJECTION_DB_NAME = process.env.OBJECTION_DB_NAME ?? 'inventory'
--- server/src/constants/knexfile.ts
+++ server/src/constants/knexfile.ts
@@ -1,10 +1,18 @@
+import {
+  OBJECTION_DB_HOST,
+  OBJECTION_DB_NAME,
+  OBJECTION_DB_PASSWORD,
+  OBJECTION_DB_PORT,
+  OBJECTION_DB_USER
+} from './env'
+
 export const knexConfig = {
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1' ?? '192.168.1.160',
-    port: 3307,
-    user: 'root',
-    password: 'wiler',
-    database: 'inventory'
+    host: OBJECTION_DB_HOST,
+    port: OBJECTION_DB_PORT,
+    user: OBJECTION_DB_USER,
+    password: OBJECTION_DB_PASSWORD,
+    database: OBJECTION_DB_NAME
   }
 }
